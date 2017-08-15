27c43e0ac09e4426c1377d0a8167319c71e66f93
Adri <amamayat@unal.edu.co>
2017-08-15 18:14:47 +0200

    Projet 15/08

--- cartonnagecrea/app/AppKernel.php
+++ cartonnagecrea/app/AppKernel.php
@@ -16,6 +16,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new cartonnagecrea\ClientBundle\cartonnagecreaClientBundle(),
+      /*       new TestBundle\TestBundle(),
+            new UserBundle\UserBundle(), */
+			new FOS\UserBundle\FOSUserBundle(),
+            new cartonnagecrea\ProduitsBundle\cartonnagecreaProduitsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
